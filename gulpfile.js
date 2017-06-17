b2ef82bf3887412da2204e89096e9587702fc641
Boldyrev Denis <boldyrev.ds@gmail.com>
2017-06-17 16:51:54 +0300

    feat: complex (#2)

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,26 @@
 var gulp = require('gulp'),
     browserSync = require('browser-sync'),
-    notify = require("gulp-notify");
+    notify = require("gulp-notify"),
+    gulpStylelint = require('gulp-stylelint');
 
 gulp.task('browser-sync', function() {
   browserSync({
     server: {
       baseDir: 'src',
-      index: 'complexes.html'
+      index: 'complex.html'
     },
     notify: false
   });
+});
+
+gulp.task('lint-css', function lintCssTask() {
+  return gulp
+    .src('src/**/*.css')
+    .pipe(gulpStylelint({
+      reporters: [
+        {formatter: 'string', console: true}
+      ]
+    }));
 });
 
 gulp.task('watch', ['browser-sync'], function() {
