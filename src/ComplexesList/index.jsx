f487ac8a2ed69356bee9d04469252b42b8f83536
Denis Boldyrev <boldyrev.ds@gmail.com>
2017-06-20 21:34:18 +0300

    refactor: rewrite ComplexesList to stateless component

---
+++ src/ComplexesList/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Banner from './Banner';
+import Content from './Content';
+
+export default () => {
+  return (
+    <div>
+      <Banner/>
+      <Content/>
+    </div>
+  );
+};
