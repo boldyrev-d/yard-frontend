b5b97a17f0c7f8d65d0126fad7abeaf2481ec180
Denis Boldyrev <boldyrev.ds@gmail.com>
2017-06-20 03:16:52 +0300

    feat: add Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './Header';
 import ComplexesList from './ComplexesList';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
@@ -11,7 +12,7 @@ class App extends Component {
           <Header />
             <Route exact path="/" component={ComplexesList} />
             {/* <Route path="/complexes/:id" component={Complex} /> */}
-          {/* <Footer /> */}
+          <Footer />
         </div>
       </Router>
     );
