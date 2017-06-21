921691a7899fdeb2406e263c1043cb6f095ad2a0
Denis Boldyrev <boldyrev.ds@gmail.com>
2017-06-21 23:18:11 +0300

    feat: add Offers

--- src/Complexes/Show/index.jsx
+++ src/Complexes/Show/index.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import Header from './Header';
 import Gallery from './Gallery';
 import Summary from './Summary';
+import Offers from './Offers';
 
 const Complex = () => (
   <div>
     <Header/>
     <Gallery/>
     <Summary/>
+    <Offers/>
   </div>
 );
 
