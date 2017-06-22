940cde7a3a25cf3e83589851b049f71d5b14c7fd
Denis Boldyrev <boldyrev.ds@gmail.com>
2017-06-23 01:25:00 +0300

    style: fix export and string interpolation

--- src/ImageSet.jsx
+++ src/ImageSet.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
-const ImageSet = (props) => {
+export default (props) => {
   const {x1, x2, x3, alt, title, ...rest} = props;
 
   return (
     <img
-      src={`${x1}`}
+      src={x1}
       srcSet={`${x2} 2x, ${x3} 3x`}
-      alt={`${alt}`} title={`${title}`}
+      alt={alt} title={title}
       {...rest}
      />
   );
 };
-
-export default ImageSet;
