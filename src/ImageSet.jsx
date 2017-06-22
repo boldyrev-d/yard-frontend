c63ede04c71e448398975c7f5a5023bfc4b3dd16
Denis Boldyrev <boldyrev.ds@gmail.com>
2017-06-23 00:01:05 +0300

    fix: fix unknown prop error

--- src/ImageSet.jsx
+++ src/ImageSet.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const ImageSet = (props) => {
+  const {x1, x2, x3, alt, title, ...rest} = props;
+
   return (
     <img
-      src={`${props.x1}`}
-      srcSet={`${props.x2} 2x, ${props.x3} 3x`}
-      alt={`${props.alt}`} title={`${props.title}`}
-      {...props}
+      src={`${x1}`}
+      srcSet={`${x2} 2x, ${x3} 3x`}
+      alt={`${alt}`} title={`${title}`}
+      {...rest}
      />
   );
 };
