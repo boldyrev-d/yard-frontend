af89a6b45f7ed38ac7c61330908a1555d051f8ca
Denis Boldyrev <boldyrev.ds@gmail.com>
2017-06-29 01:05:09 +0300

    refactor: fix condition, rewrite to arrow function, use destructuring for image id

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
 /* eslint-disable import/prefer-default-export */
 
-export function getImageUrl(image, imageSize) {
-  if (!image) {
-    return 'http://via.placeholder.com/512/?text=%D0%9D%D0%B5%D1%82+%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F';
+export const getImageUrl = ({ id }, imageSize) => {
+  if (id) {
+    return `https://images.jqestate.ru/${id}-jqestate-${imageSize}`;
   }
 
-  return `https://images.jqestate.ru/${image.id}-jqestate-${imageSize}`;
-}
+  return 'http://via.placeholder.com/512/?text=%D0%9D%D0%B5%D1%82+%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F';
+};
