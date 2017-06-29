d2996eef15de571e5183c1636e19c27faa5ccf40
Denis Boldyrev <boldyrev.ds@gmail.com>
2017-06-29 20:40:35 +0300

    chore: add flow to api

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 
+/* @flow */
 const API_BASE_URL = 'https://api.jqestate.ru/v1';
 
-export const get = resource => fetch(API_BASE_URL + resource).then(res => res.json());
+export const get = (resource: string): Promise<any> =>
+  fetch(API_BASE_URL + resource).then(res => res.json());
