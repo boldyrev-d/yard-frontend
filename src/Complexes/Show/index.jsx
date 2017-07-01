5219c31482dcf1ed92d09982aa4b13e06d9dc0a4
Denis Boldyrev <boldyrev.ds@gmail.com>
2017-07-01 10:58:00 +0300

    refactor: add models for Complex

--- src/Complexes/Show/index.jsx
+++ src/Complexes/Show/index.jsx
@@ -12,7 +12,96 @@ import { get } from '../../api';
 class Complex extends Component {
   state = {};
 
-  state: Object;
+  state: {
+    adjacentTerritory: Object,
+    name: string,
+    updatedAt: string,
+    location: {
+      latitude: string,
+      districtId: number,
+      localityId: number,
+      subLocalityName: string,
+      subwayIds: Array<number>,
+      countryName: string,
+      districtName: string,
+      longitude: string,
+      postalCode: number,
+      countryId: number,
+      subLocalityId: number,
+      house: string,
+      localityName: string,
+      street: string,
+      regionId: number,
+      regionName: string,
+    },
+    state: string,
+    statistics: {
+      resalePropertiesCount: number,
+      price: {
+        from: {
+          usd: number,
+          eur: number,
+          rub: number,
+        },
+        to: {
+          usd: number,
+          eur: number,
+          rub: number,
+        },
+      },
+      totalResaleArea: {
+        from: number,
+        to: number,
+      },
+      resalePrice: {
+        from: {
+          usd: number,
+          eur: number,
+          rub: number,
+        },
+        to: {
+          usd: number,
+          eur: number,
+          rub: number,
+        },
+      },
+      propertiesCount: number,
+      totalPrimaryArea: {
+        from: number,
+        to: number,
+      },
+      primaryPrice: {
+        from: {
+          usd: number,
+          eur: number,
+          rub: number,
+        },
+        to: {
+          usd: number,
+          eur: number,
+          rub: number,
+        },
+      },
+      primaryPropertiesCount: number,
+      totalArea: {
+        from: number,
+        to: number,
+      },
+    },
+    purchaseTimeConditions: Object,
+    linkedContactIds: Array<number>,
+    responsibleUser: {
+      id: number,
+      departmentId: number,
+      divisionId: number,
+    },
+    createdByUserId: number,
+    id: number,
+    updatedByUserId: number,
+    createdAt: string,
+    images: Array<Object>,
+    accreditors: Array<Object>,
+  };
 
   componentDidMount() {
     this.loadComplex(this.props.match.params.id);
