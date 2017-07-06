395b92a2c0b41e822d9b50d0ab88cfa9b8e6fd5b
Denis Boldyrev <boldyrev.ds@gmail.com>
2017-07-06 16:46:15 +0300

    feat: change api urls

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,7 @@
 
 export const getImageUrl = (image: Object, size: number = 512): string => {
   if (image) {
-    return `https://images.jqestate.ru/${image.id}-jqestate-${size}`;
+    return `https://s3-eu-central-1.amazonaws.com/yard-images/${image.id}-${size}`;
   }
 
   return 'http://via.placeholder.com/512/?text=%D0%9D%D0%B5%D1%82+%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F';
